5b70c0c038f0c11041b885c1668e890b83ac3a0a
SAIDHEERAJ-TOXIC <saidheeraj@example.com>
2026-08-21 06:54:45 +0530

    revert: remove multi-language preloader

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,5 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Preloader from "@/components/Preloader";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -16,7 +15,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${inter.className} min-h-full flex flex-col`}>
-        <Preloader />
         {children}
       </body>
     </html>
